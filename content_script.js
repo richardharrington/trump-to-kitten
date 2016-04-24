046d8c9dd46d8f26b426c93b930088d7353a058f
Richard Harrington <harrington.richard@gmail.com>
2016-04-24 17:08:38 -0400

    cleaned up a bit, made more functional

--- content_script.js
+++ content_script.js
@@ -1,35 +1,40 @@
+function onceDOMContentLoaded(f) {
+  if (document.readyState === 'interactive') {
+    f();
+  } else {
+    document.addEventListener('DOMContentLoaded', f);
+  }
+}
+
+function arrayify(quasiArray) {
+  var arr = [];
+  for (var i = 0; i < quasiArray.length; i++) {
+    arr.push(quasiArray[i]);
+  }
+  return arr;
+}
+
+function foundTheDonald(image) {
+  var attrs = arrayify(image.attributes);
+  return attrs.some(function(attr) {
+    return attr.nodeValue.match(/trump|the\s*?donald/i);
+  });
+}
+
 function meowify(image) {
-  var attrs = image.attributes;
-  for (var i = 0; i < attrs.length; i++) {
-    if (attrs[i].nodeValue.match(/trump|the\s*?donald/i)) {
-      image.removeAttribute('srcset');
-      image.setAttribute('src', 'http://placekitten.com/' + image.clientWidth + '/' + image.clientHeight);
-      return;
-    }
-  }
+  image.removeAttribute('srcset');
+  image.removeAttribute('src');
+  image.setAttribute('src', 'http://placekitten.com/' + image.clientWidth + '/' + image.clientHeight);
 }
 
-function meowifyAllImages() {
+function meowifyAllDonalds() {
   var images = document.querySelectorAll('img');
-  for (var i = 0; i < images.length; i++){
-    var img = images[i];
-
-    if (img.complete) {
-      meowify(img);
-      continue;
+  arrayify(images).forEach(function(image) {
+    if (foundTheDonald(image)) {
+      meowify(image);
     }
-
-    (function(image){
-      image.addEventListener('load', function(){
-        meowify(image);
-      });
-    })(img);
-  }
+  });
 }
 
-if (document.readyState === 'interactive') {
-  meowifyAllImages();
-} else {
-  document.addEventListener('DOMContentLoaded', meowifyAllImages);
-}
+onceDOMContentLoaded(meowifyAllDonalds);
 
